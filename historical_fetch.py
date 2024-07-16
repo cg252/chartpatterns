b2848274824c4d6c571c79b52e7cbffcbd73de21
Colter G <127794196+cg252@users.noreply.github.com>
2024-07-15 22:33:35 -0400

    progress...?

--- historical_fetch.py
+++ historical_fetch.py
@@ -3,17 +3,17 @@ import numpy as np
 import pandas as pd
 
 #TICKER = "EURUSD=X"
-TICKER = "SPY"
-Position_duration = 3
+TICKER = "ES=F"
+Position_duration = 5
 # number of candles before selling
 #start-  2022-07-9
 
 
 # buy sell column // do nothing 
 
-arr = yf.download(TICKER, start="2023-07-15", interval="1h")
+arr = yf.download(TICKER, start="2022-07-25", interval="1h")
 np.seterr(divide='ignore', invalid='ignore')
-
+arr.index = arr.index.tz_convert("US/Eastern")
 
 #print(arr.index[519])
 
@@ -54,8 +54,8 @@ for i in range(len(arr)):
         
     highval = last60hsum/60
     lowval = last60lsum/60
-    highYBRList.append(highval/60)
-    lowYBRList.append(lowval/60)
+    highYBRList.append(highval)
+    lowYBRList.append(lowval)
 
     #append integer value based on the positon of close relative to YBR range
     # 1 for bullish, 2 for bearish, 0 for neutral
@@ -67,6 +67,27 @@ for i in range(len(arr)):
         YBRposList.append(0)
     else:
          YBRposList.append(0)
+
+#finds whether price recently crossed YBR to upside. 
+YBRCrossoverList = []
+for i in range(len(arr)):
+    closeTD = arr['Close'].iloc[i]
+    close4d = arr['Close'].iloc[i-4]
+    YBRhighcur = highYBRList[i]
+    YBRhigh4d = highYBRList[i-4]
+
+    if closeTD > YBRhighcur:
+        if close4d < YBRhigh4d:
+            YBRCrossoverList.append(1)
+        else:
+             YBRCrossoverList.append(0)
+    elif closeTD < YBRhighcur:
+        if close4d > YBRhigh4d:
+            YBRCrossoverList.append(2)
+        else:
+            YBRCrossoverList.append(0)
+    else:
+         YBRCrossoverList.append(0)
 
 PCS = EMA(PCList, 25)
 PCDS = EMA(PCS, 13)
@@ -88,10 +109,12 @@ TSIRelativePosition = []
 ResultsList = []
 candlestickPattern = []
 
-#might be 0 for hold, or 1 for buy
+#0: nothing
+#1-5: buy varying position sizes
 buysignal = []
-
 for i in range(len(TSI)):
+    buystrength = 0
+
     #candle identification
     closeCur = arr["Close"].iloc[i]
     highCur =  arr["High"].iloc[i]
@@ -109,40 +132,36 @@ for i in range(len(TSI)):
     avglen5 = (fulllen + abs( arr["High"].iloc[i-1]- arr["Low"].iloc[i-1]) + abs( arr["High"].iloc[i-2] - arr["Low"].iloc[i-2])
     + abs( arr["High"].iloc[i-3]- arr["Low"].iloc[i-3]) + abs( arr["High"].iloc[i-4]- arr["Low"].iloc[i-4]))/5
 
-    if (topwick < 0.15*fulllen) and (bottomwick>1.35*bodylen) and (min(openCur, closeCur) > lowCur) and (fulllen > 0.8*avglen5):
-        if tsiCur > sigCur:
-            candlestickPattern.append('hammer')
-        else:
-            candlestickPattern.append('hammer')
+    if (topwick < 0.15*fulllen) and (bottomwick>1.35*bodylen) and (min(openCur, closeCur) > lowCur) and (fulllen > 0.9*avglen5):
+        candlestickPattern.append('hammer')
+        #add 1 to buystrength if hammer is present at current tracing location and price has recently crossed ybr to upside
+        if (YBRCrossoverList[i] == 1):
+            buystrength += 1
+    elif (bottomwick < 0.15*fulllen) and (topwick > 1.35*bodylen) and (max(openCur, closeCur) < highCur) and (fulllen > 0.9*avglen5):
+        candlestickPattern.append('inverted_hammer')
     else:
         candlestickPattern.append('')
 
-
-
     ######
-
 
     TSIcur = TSI[i]
     TSI1d = TSI[i-1]
-    TSI2d = TSI[i-2]
+    TSI4d = TSI[i-4]
     SIGcur = SIGNAL[i]
-
     try:
-        SIG1d = SIGNAL[i-1]
-        SIG2d = SIGNAL[i-2]
+        SIG4d = SIGNAL[i-4]
     except:
-        SIG1d = 0
-        SIG2d = 0
+        SIG4d = SIGNAL[i]
 
     TSIRelativePosition.append(TSIcur/SIGcur)
 
     if TSIcur > SIGcur:
-        if TSI2d < SIGcur:
+        if TSI4d < SIG4d:
             TSICrossover.append(1)
         else:
              TSICrossover.append(0)
     elif TSIcur < SIGcur:
-        if TSI2d > SIGcur:
+        if TSI4d > SIG4d:
             TSICrossover.append(2)
         else:
             TSICrossover.append(0)
@@ -163,17 +182,17 @@ for i in range(len(TSI)):
     else:
          pr = 0
     ResultsList.append(pr)
+
+    buysignal.append(buystrength)
      
 
-data = {"Open": arr['Open'],
-        "Close": arr['Close'],
-        "High": arr['High'], 
-        "Low": arr['Low'],
-        "TSI": TSI,
+data = {"Close": arr['Close'],
         "TSI_Position": TSIRelativePosition,
         "TSI_Crossover": TSICrossover,
         "YBR_Position": YBRposList,
+        "YBR_Crossover": YBRCrossoverList,
         "Hammer": candlestickPattern,
+        "Signal": buysignal,
         "Results": ResultsList
         }
 
